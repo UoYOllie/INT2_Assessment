e3acac51c522a8bc28d5ed3440f9348db7d76299
UoYOllie <oj613@york.ac.uk>
2023-05-01 15:29:05 +0100

    testing things

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ import tensorflow_datasets as tfds
 ds = tfds.load('oxford_flowers102', split='train', shuffle_files=True)
 assert isinstance(ds, tf.data.Dataset)
 
+ds = ds.batch(32).prefetch(tf.data.AUTOTUNE)
+for example in ds.take(1):
+  image, label = example["image"], example["label"]
+  print(image, label)
+
+
+
+# plt.figure()
+# plt.imshow(ds.images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
 # builder = tfds.builder('mnist')
 # builder.download_and_prepare()
 # ds = builder.as_dataset(split='train', shuffle_files=True)
 
-
-ds.get_single_element("image")
