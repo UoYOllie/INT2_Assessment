25ed9d2165f638162a2859fdad7500d4c7ce4487
sm2945 <sm2945@york.ac.uk>
2023-05-01 15:03:31 +0100

    Loading the dataset according to tensorflow guide.

--- test.py
+++ test.py
@@ -1,14 +1,35 @@
+
+import tensorflow as tf
+# import tensorflow_datasets as tfds
+
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
 
-import tensorflow_datasets as tfds
-
-ds = tfds.load('oxford_flowers102', split='train', shuffle_files=True)
-assert isinstance(ds, tf.data.Dataset)
+# ds = tfds.load('oxford_flowers102', split='train', shuffle_files=True)
+# assert isinstance(ds, tf.data.Dataset)
 
 # builder = tfds.builder('mnist')
 # builder.download_and_prepare()
 # ds = builder.as_dataset(split='train', shuffle_files=True)
 
-print(ds)
+# .load and .builder serve the same function. tensorflow website says .load is easier
+
+
+oxford_flowers102 = tf.keras.datasets.oxford_flowers102
+(train_images, train_labels), (test_images, test_labels) = oxford_flowers102.load_data()
+
+train_images.shape
+len(train_labels)
+train_labels
+test_images.shape
+len(test_labels)
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+
+# x_train, x_test = x_train / 255.0, x_test / 255.0
+
