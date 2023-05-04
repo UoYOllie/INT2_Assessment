c8dbd73b194c3f1002e60f43a7d326f1bb64b0d3
Oliver <oj613@york.ac.uk>
2023-05-04 15:03:44 +0100

    Neural Network

---
+++ CustomCallback.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+from tensorflow import keras
+from matplotlib import plt
+
+
+class CustomCallback(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        keys = list(logs.keys())
+        print("Starting training; got log keys: {}".format(keys))
+
+        self.metrics = {}
+        for metric in logs:
+            self.metrics[metric] = []
+
+    def on_train_end(self, logs=None):
+        pass
+
+    def on_epoch_begin(self, epoch, logs=None):
+        pass
+
+    def on_epoch_end(self, epoch, logs=None):
+        for metric in logs:
+            if metric in self.metrics:
+                self.metrics[metric].append(logs.get(metric))
+            else:
+                self.metrics[metric] = [logs.get(metric)]
+
+        metrics = [x for x in logs if 'val' not in x]
+
+        f, axs = plt.subplots(1, len(metrics), figsize=(15, 5))
+        #clear_output(wait=True)
+
+        for i, metric in enumerate(metrics):
+            axs[i].plot(range(1, epoch + 2),
+                        self.metrics[metric],
+                        label=metric)
+            if logs['val_' + metric]:
+                axs[i].plot(range(1, epoch + 2),
+                            self.metrics['val_' + metric],
+                            label='val_' + metric)
+
+            axs[i].legend()
+            axs[i].grid()
+
+        plt.tight_layout()
+        plt.show()
+
+
+    def on_test_begin(self, logs=None):
+        pass
+
+    def on_test_end(self, logs=None):
+        pass
+
+    def on_predict_begin(self, logs=None):
+        pass
+
+    def on_predict_end(self, logs=None):
+        pass
+
+    def on_train_batch_begin(self, batch, logs=None):
+        pass
+
+    def on_train_batch_end(self, batch, logs=None):
+        pass
+
+    def on_test_batch_begin(self, batch, logs=None):
+        pass
+
+    def on_test_batch_end(self, batch, logs=None):
+        pass
+
+    def on_predict_batch_begin(self, batch, logs=None):
+        pass
+
+    def on_predict_batch_end(self, batch, logs=None):
+        pass
--- main.py
+++ main.py
@@ -1,27 +1,80 @@
+import keras.callbacks
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import PIL
+import CustomCallback
+
 import tensorflow as tf
 
 import tensorflow_datasets as tfds
 
+batch_size = 64
+epochs = 10
+
+
+def resize_image(image, label):
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, (256, 256))
+    return (image, label)
+
+
 ds = tfds.load('oxford_flowers102', split='train', shuffle_files=True)
+ds_val = tfds.load('oxford_flowers102', split="validation", shuffle_files=True)
+
 assert isinstance(ds, tf.data.Dataset)
+assert isinstance(ds_val, tf.data.Dataset)
 
-ds = ds.batch(32).prefetch(tf.data.AUTOTUNE)
-for example in ds.take(1):
-  image, label = example["image"], example["label"]
-  print(image, label)
+ds = ds.map(lambda b: (b['image'] / 255, b['label']))
+ds_val = ds_val.map(lambda a: (a['image'] / 255, a['label']))
 
+ds_val = ds_val.map(resize_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(batch_size).prefetch(
+    tf.data.experimental.AUTOTUNE)
 
+ds = ds.map(resize_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(batch_size).prefetch(
+    tf.data.experimental.AUTOTUNE)
 
-# plt.figure()
-# plt.imshow(ds.images[0])
-# plt.colorbar()
-# plt.grid(False)
-# plt.show()
+model = tf.keras.models.Sequential()
 
-# builder = tfds.builder('mnist')
-# builder.download_and_prepare()
-# ds = builder.as_dataset(split='train', shuffle_files=True)
+# Input Shape : width, height, colour channels
+model.add(tf.keras.layers.RandomFlip("horizontal", input_shape=(256, 256, 3)))
+model.add(tf.keras.layers.RandomRotation(0.4))
+model.add(tf.keras.layers.RandomZoom(0.3))
+#model.add(tf.keras.layers.RandomBrightness(0.1))
+
+model.add(tf.keras.layers.Conv2D(16, (3, 2), 1, padding='same', activation='relu', input_shape=(256, 256, 3)))
+model.add(tf.keras.layers.MaxPooling2D())
+
+model.add(tf.keras.layers.Conv2D(32, (2, 3), 1, padding='same', activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D())
+
+model.add(tf.keras.layers.Conv2D(64, (3, 3), 1, padding='same', activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D())
+
+model.add(tf.keras.layers.Dropout(0.2))
+
+model.add(tf.keras.layers.Flatten())
+
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(102))
+
+model.compile('adam', loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+
+model.summary()
+
+logdir = "logs"
+tensorboard_callback = CustomCallback(log_dir=logdir)
+
+hist = model.fit(ds, epochs=1, validation_data=ds_val)
+plt.figure()
+
+# plt.plot(hist.history['loss'], color='teal', label='training-loss')
+plt.plot(hist.history['accuracy'], color='orange', label='training-accuracy')
+# plt.plot(hist.history['val_loss'], color='blue', label='val_loss')
+plt.plot(hist.history['val_accuracy'], color='red', label='val_accuracy')
+plt.title('Training Loss & Accuracy')
+plt.legend(loc="upper left")
+plt.show()
+hist = model.fit(ds, epochs=epochs, validation_data=ds_val)
+
 
