5c7ee42eef303427e98df3e8e375a8f0316c9e00
Oliver <oj613@york.ac.uk>
2023-05-04 21:43:35 +0100

    Modified DNN

--- CustomCallback.py
+++ CustomCallback.py
@@ -1,16 +1,23 @@
 import tensorflow as tf
+from keras.utils import io_utils
 from tensorflow import keras
-from matplotlib import plt
+import matplotlib.pyplot as plt
 
 
 class CustomCallback(keras.callbacks.Callback):
+
+
     def on_train_begin(self, logs={}):
         keys = list(logs.keys())
         print("Starting training; got log keys: {}".format(keys))
 
+
+
         self.metrics = {}
         for metric in logs:
             self.metrics[metric] = []
+
+
 
     def on_train_end(self, logs=None):
         pass
@@ -19,6 +26,7 @@ class CustomCallback(keras.callbacks.Callback):
         pass
 
     def on_epoch_end(self, epoch, logs=None):
+
         for metric in logs:
             if metric in self.metrics:
                 self.metrics[metric].append(logs.get(metric))
@@ -27,23 +35,28 @@ class CustomCallback(keras.callbacks.Callback):
 
         metrics = [x for x in logs if 'val' not in x]
 
-        f, axs = plt.subplots(1, len(metrics), figsize=(15, 5))
+        if epoch == 0:
+            self.f, self.axs = plt.subplots(1, len(self.metrics), figsize=(15, 5))
+
         #clear_output(wait=True)
 
         for i, metric in enumerate(metrics):
-            axs[i].plot(range(1, epoch + 2),
+            self.axs[i].plot(range(1, epoch + 2),
                         self.metrics[metric],
                         label=metric)
             if logs['val_' + metric]:
-                axs[i].plot(range(1, epoch + 2),
+                self.axs[i].plot(range(1, epoch + 2),
                             self.metrics['val_' + metric],
                             label='val_' + metric)
 
-            axs[i].legend()
-            axs[i].grid()
+            self.axs[i].legend()
+            self.axs[i].grid()
 
         plt.tight_layout()
-        plt.show()
+        plt.show(block)
+        # plt.ion()
+        # plt.pause(0.01)
+        # plt.show()
 
 
     def on_test_begin(self, logs=None):
--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import PIL
-import CustomCallback
 
 import tensorflow as tf
 
 import tensorflow_datasets as tfds
 
 batch_size = 64
-epochs = 10
+epochs = 64
 
 
 def resize_image(image, label):
@@ -38,34 +37,41 @@ model = tf.keras.models.Sequential()
 
 # Input Shape : width, height, colour channels
 model.add(tf.keras.layers.RandomFlip("horizontal", input_shape=(256, 256, 3)))
-model.add(tf.keras.layers.RandomRotation(0.4))
-model.add(tf.keras.layers.RandomZoom(0.3))
-#model.add(tf.keras.layers.RandomBrightness(0.1))
+#model.add(tf.keras.layers.RandomFlip("vertical"))
 
-model.add(tf.keras.layers.Conv2D(16, (3, 2), 1, padding='same', activation='relu', input_shape=(256, 256, 3)))
+model.add(tf.keras.layers.RandomRotation(0.8))
+model.add(tf.keras.layers.RandomZoom(0.2))
+model.add(tf.keras.layers.RandomTranslation(0.3, 0.3))
+
+# model.add(tf.keras.layers.RandomBrightness(0.1))
+
+model.add(tf.keras.layers.Conv2D(16, (3, 3), 1, padding='same', activation='relu', input_shape=(256, 256, 3)))
 model.add(tf.keras.layers.MaxPooling2D())
 
-model.add(tf.keras.layers.Conv2D(32, (2, 3), 1, padding='same', activation='relu'))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), 1, padding='same', activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D())
 
 model.add(tf.keras.layers.Conv2D(64, (3, 3), 1, padding='same', activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D())
 
-model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Conv2D(128, (3, 3), 1, padding='same', activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D())
+
+model.add(tf.keras.layers.Dropout(0.3))
 
 model.add(tf.keras.layers.Flatten())
 
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(102))
+model.add(tf.keras.layers.Dense(512, activation='relu'))
+model.add(tf.keras.layers.Dense(102, activation='softmax'))
 
 model.compile('adam', loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
 model.summary()
 
 logdir = "logs"
-tensorboard_callback = CustomCallback(log_dir=logdir)
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
-hist = model.fit(ds, epochs=1, validation_data=ds_val)
+hist = model.fit(ds, epochs=epochs, validation_data=ds_val)
 plt.figure()
 
 # plt.plot(hist.history['loss'], color='teal', label='training-loss')
@@ -75,6 +81,6 @@ plt.plot(hist.history['val_accuracy'], color='red', label='val_accuracy')
 plt.title('Training Loss & Accuracy')
 plt.legend(loc="upper left")
 plt.show()
-hist = model.fit(ds, epochs=epochs, validation_data=ds_val)
+
 
 
